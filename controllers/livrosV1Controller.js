7a4d16c2254f396f582fd061144797a97b5fbb8a
felipetexa <felipe.teixeira91@hotmail.com>
2022-07-25 21:24:31 -0300

    CRUD de adição, edição e exclusão de livro

--- controllers/livrosV1Controller.js
+++ controllers/livrosV1Controller.js
@@ -1,4 +1,5 @@
 const { Livro } = require('../models');
+const { Op } = require("sequelize");
 
 const livrosV1Controller = {
   showAllBooks: async (req, res) => {
@@ -22,11 +23,77 @@ const livrosV1Controller = {
     }
   },
   store: async (req, res) => {
-    const data = req.body;
+    try {
+      const { titulo, quantidade_paginas, autor, ano_lancamento, estoque } = req.body;
 
-    //criar logica
+      const verificarSeExiste = await Livro.findOne({
+        where: {
+          [Op.and]: [{ titulo }, { autor }]
+        }
+      });
 
-    return res.status(201).json(data);
+      if(verificarSeExiste) {
+        return res.status(422).json({ message: 'Livro já existe' });
+      }
+
+      const novoLivro = await Livro.create({
+        titulo,
+        quantidade_paginas,
+        autor,
+        ano_lancamento,
+        estoque
+      });
+
+      return res.status(201).json({ message: 'Livro cadastrado com sucesso', livro: novoLivro });
+    }catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: true, message: 'Sistema indisponível, tente novamente mais tarde' });
+    }
+  },
+  edit: async (req, res) => {
+    try {
+      const { id  } = req.params;
+      const { titulo, quantidade_paginas, autor, ano_lancamento, estoque } = req.body;
+
+      const livro = await Livro.findByPk(id);
+
+      if(!livro) { return res.status(404).json({ error: true, message: 'Livro não encontrado' }); }
+
+      await Livro.update({
+        titulo,
+        quantidade_paginas,
+        autor,
+        ano_lancamento,
+        estoque
+      }, {where: {id}} );
+
+      const livroAtualizado = await Livro.findByPk(id);
+      return res.status(200).json({ message: 'Livro atualizado com sucesso', livro: livroAtualizado });
+
+  } catch(error) {
+    console.log(error);
+    return res.status(500).json({ error: true, message: 'Sistema indisponível, tente novamente mais tarde' })
+  }
+},
+  delete: async (req, res) => {
+    try{
+      const { id } = req.params;
+
+      const livro = await Livro.findByPk(id);
+
+      if(!livro) {
+        return res.status(404).json({
+          error: true, message: 'Livro não encontrado'
+        });
+      }
+
+      await Livro.destroy({where: {id}});
+
+      return res.status(200).json({ message: 'Livro excluído com sucesso' });
+    }catch(error){
+      console.log(error);
+      return res.status(500).json({ error: true, message: 'Sistema indisponível, tente novamente mais tarde' })
+    }
   }
 }
 
